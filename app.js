681ef113bcbf31e6e7e23ae168999bcabf47f6fe
Jose Carranco <carranco.jose.r@gmail.com>
2020-05-08 01:46:00 -0500

    GET a quote, GET all Quotes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/greetings', (req, res)=>{
-    res.json({greeting: 'Hellow World!'});
+app.get('/quotes', (req, res)=>{
+    res.json(data);
 });
-
 //Send a GET request to /quotes READ(view) a list of quotes
+app.get('/quotes/:id', (req, res)=>{
+    const quote = data.quotes.find(quote => quote.id == req.params.id);
+    res.json(quote);
+});
 //Send a GET request to /quotes/:id to READ(view) a quote
 //Send a POST request to /quotes/ CREATE a quote
 //Send a PUT request to /quotes/:id UPDATE(edit) a quote
@@ -13,3 +16,23 @@ app.get('/greetings', (req, res)=>{
 //Send a GET request to /quotes/quote/random READ (view) a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
+
+const data = {
+    quotes: [
+        {
+            id: 8721,
+            quote: "We must accept finite disappointment, but we must never lose infinite hope.",
+            author: "Martin Luther King"
+        },
+        {
+            id: 5779,
+            quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
+            author: "Yvonne Pierre"
+        },
+        {
+            id: 3406,
+            quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
+            author: "Barbara DeAngelis"
+        }
+    ]
+  }
