05ca7ebcb7c858b8fe33c6353324310c6004f935
carranj <carranco.jose.r@gmail.com>
2020-05-18 22:50:02 -0500

    Refactor the Get All Quotes Route

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
+const records = require('./records');
+
 app.get('/quotes', (req, res)=>{
-    res.json(data);
+    const quotes = records.getQuotes();
+    res.json(quotes);
 });
 //Send a GET request to /quotes READ(view) a list of quotes
 app.get('/quotes/:id', (req, res)=>{
-    const quote = data.quotes.find(quote => quote.id == req.params.id);
-    res.json(quote);
+    
 });
 //Send a GET request to /quotes/:id to READ(view) a quote
 //Send a POST request to /quotes/ CREATE a quote
@@ -16,23 +18,3 @@ app.get('/quotes/:id', (req, res)=>{
 //Send a GET request to /quotes/quote/random READ (view) a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
-
-const data = {
-    quotes: [
-        {
-            id: 8721,
-            quote: "We must accept finite disappointment, but we must never lose infinite hope.",
-            author: "Martin Luther King"
-        },
-        {
-            id: 5779,
-            quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-            author: "Yvonne Pierre"
-        },
-        {
-            id: 3406,
-            quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-            author: "Barbara DeAngelis"
-        }
-    ]
-  }
