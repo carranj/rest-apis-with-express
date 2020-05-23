161d6432170cf6b49e9d7d9f22cdbac2673545fb
carranj <carranco.jose.r@gmail.com>
2020-05-23 12:09:58 -0500

    HTTP Status Codes

--- app.js
+++ app.js
@@ -14,9 +14,15 @@ app.get('/quotes', async (req, res)=>{
 app.get('/quotes/:id', async (req, res)=>{
     try{
         const quote = await records.getQuote(req.params.id);
-        res.json(quote);
+        if(quote){
+            res.json(quote);
+        } else {
+            res.status(404).json({message: "Quote not found."});
+        }
+        
     } catch (err){
-        res.json({message: err.message});
+        res.status(500)
+        .json({message: err.message});
     }
     
 });
@@ -24,13 +30,17 @@ app.get('/quotes/:id', async (req, res)=>{
 //Send a POST request to /quotes/ CREATE a quote
 app.post('/quotes', async (req, res)=>{
     try{
-        const quote = await records.createQuote({
-            quote:req.body.quote,
-            author: req.body.author
-        });
-        res.json(quote);
+        if(req.body.author && req.body.quote){
+            const quote = await records.createQuote({
+                quote:req.body.quote,
+                author: req.body.author
+            });
+            res.status(201).json(quote)
+        } else {
+            res.status(400).json({message: "Quote and author required."});
+        }
     } catch (err){
-        res.json({message: err.message})
+        res.status(500).json({message: err.message})
     }
 });
 //Send a PUT request to /quotes/:id UPDATE(edit) a quote
